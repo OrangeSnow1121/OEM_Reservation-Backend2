9edad9e7d77545d5f4e979151b1701b6cfa72ef0
OrangeSnow1121 <att.jaketest3648@gmail.com>
2025-07-22 20:45:21 -0500

    server.js

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+const authRoutes = require('./auth'); 
+
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -13,6 +15,8 @@ const MONGO_URI = process.env.MONGO_URI;
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.error(err));
+
+app.use('/api/auth', authRoutes);
 
 // Example endpoint
 app.get("/", (req, res) => {
